d6f24ba451e05fee6f3e8e34e14052abf97c15b9
mcnether <46548077+mcnether@users.noreply.github.com>
2019-06-04 19:58:31 -0600

    Add files via upload

---
+++ js/variable.js
@@ -0,0 +1,10 @@
+console.log("Instructional Designer")
+
+var element = document.getElementById("view_home");
+    console.log(element);
+
+var element = document.getElementById("view_portfolio");
+    console.log(element);
+
+var element = document.getElementById("view_contact");
+    console.log(element);
