127698d5859e5cac87999ca1a98dcbde8725658f
mcnether <46548077+mcnether@users.noreply.github.com>
2019-07-02 19:40:29 -0600

    Add files via upload

---
+++ js/listener.js
@@ -0,0 +1,13 @@
+console.log("listener is loaded...");
+
+view_home.addEventListener("click", function(){
+   console.log("User clicked on home button"); 
+});
+
+view_portfolio.addEventListener("click", function(){
+    console.log ("User clicked on portfolio button");
+});
+
+view_contact.addEventListener("click", function(){
+    console.log ("User clicked on contact button");
+});
--- js/variable.js
+++ js/variable.js
@@ -1,10 +1,15 @@
 console.log("Instructional Designer")
 
-var element = document.getElementById("view_home");
-    console.log(element);
+//Each variable should have a unique name.
+var element_1 = document.getElementById("view_home");
+    console.log(element_1);
 
-var element = document.getElementById("view_portfolio");
-    console.log(element);
+var element_2 = document.getElementById("view_portfolio");
+    console.log(element_2);
 
-var element = document.getElementById("view_contact");
-    console.log(element);
+var element_3 = document.getElementById("view_contact");
+    console.log(element_3);
+
+// You can assign views to variables also... as in here.
+var element_4 = document.getElementById("home");
+    console.log(element_4);
