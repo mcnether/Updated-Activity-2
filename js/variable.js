85ce4b4020cfc5443fcf83980e0e11fe9bc18daa
mcnether <46548077+mcnether@users.noreply.github.com>
2019-07-09 18:30:30 -0600

    Add files via upload

--- js/variable.js
+++ js/variable.js
@@ -2,16 +2,16 @@ console.log("Instructional Designer")
 
 //Each variable should have a unique name.
 var view_home = document.getElementById("view_home");
-    console.log(element_1);
+    console.log(view_home);
 
 var view_aboutme = document.getElementById("view_aboutme");
-    console.log(element_2);
+    console.log(view_aboutme);
 
 var view_portfolio = document.getElementById("view_portfolio");
-    console.log(element_3);
+    console.log(view_portfolio);
 
 var view_contact = document.getElementById("view_contact");
-    console.log(element_4);
+    console.log(view_contact);
 
 
 
